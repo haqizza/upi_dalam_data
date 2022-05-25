123d826394789c2497b9180a584720ea4a4e3491
ayshaaliaisk <74904435+ayshaaliaisk@users.noreply.github.com>
2022-05-25 10:20:43 +0700

    news

---
+++ lib/screens/news.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:upi_dalam_data/widgets/topbar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key,}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => readJson());
+  }
+  List _items = [];
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/data_berita.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["items"];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TopBar(
+        title: const Text('News',
+                      style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+                    
+        appBar: AppBar(
+        ),
+        widgets: const <Widget>[Icon(Icons.abc_rounded)],
+        isLeading: false,
+        leading: Image.asset(
+          'images/logoUPI_32.png',
+          // fit: BoxFit.fitHeight,
+        ),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+
+            _items.isNotEmpty? Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Image.network("https://storage.googleapis.com/cv-skn-bucket/pemvis/kelompok-1/Picture1-4.png", fit: BoxFit.cover, height: 150, width: 150,),
+                      title: Text(_items[index]["judul"]),
+                      subtitle: Text(_items[index]["tanggal"]),
+                    )
+                  );
+                },)
+            )
+            : Container()
+        ],)
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: const Color(0xFFFF0404),
+        unselectedItemColor: const Color(0xff000000),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insights_outlined),
+            label: 'Dashboard'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.view_quilt_outlined),
+            label: 'Highlight'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_outlined),
+            label: 'Notifications',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
